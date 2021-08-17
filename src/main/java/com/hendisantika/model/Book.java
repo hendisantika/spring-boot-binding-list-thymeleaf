dbbc91b63b29270b53bcccc81d7589229890a73c
Hendi Santika <hendisantika@gmail.com>
2021-08-18 04:43:25 +0700

    #3 - Add Book Model

---
+++ src/main/java/com/hendisantika/model/Book.java
@@ -0,0 +1,21 @@
+package com.hendisantika.model;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-binding-list-thymeleaf
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 18/08/21
+ * Time: 04.42
+ */
+@Data
+public class Book {
+    private long id;
+
+    private String title;
+
+    private String author;
+}
