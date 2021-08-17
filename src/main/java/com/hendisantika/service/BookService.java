505ed487e58dc5f628ec2b693b7d3513dd3c34f7
Hendi Santika <hendisantika@gmail.com>
2021-08-18 04:51:53 +0700

    #6 - Add saveAll in Book Service

--- src/main/java/com/hendisantika/service/BookService.java
+++ src/main/java/com/hendisantika/service/BookService.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,5 +25,27 @@ public class BookService {
 
     public List<Book> findAll() {
         return new ArrayList<>(booksDB.values());
+    }
+
+    public void saveAll(List<Book> books) {
+        long nextId = getNextId();
+        for (Book book : books) {
+            if (book.getId() == 0) {
+                book.setId(nextId++);
+            }
+        }
+
+        Map<Long, Book> bookMap = books.stream()
+                .collect(Collectors.toMap(Book::getId, Function.identity()));
+
+        booksDB.putAll(bookMap);
+    }
+
+    private Long getNextId() {
+        return booksDB.keySet()
+                .stream()
+                .mapToLong(value -> value)
+                .max()
+                .orElse(0) + 1;
     }
 }
