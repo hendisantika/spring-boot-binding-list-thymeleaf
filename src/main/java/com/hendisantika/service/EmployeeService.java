65c89b7ae8bd5dde21a9106d6de5fa4ad977e30e
Hendi Santika <hendisantika@gmail.com>
2021-08-18 05:13:11 +0700

    #21 - Add EmployeeService

---
+++ src/main/java/com/hendisantika/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.hendisantika.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-binding-list-thymeleaf
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 18/08/21
+ * Time: 05.12
+ */
+@Service
+public class EmployeeService {
+
+}
