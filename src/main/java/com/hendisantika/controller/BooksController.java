2cd8059bef6d01d06b0ddef29e5e4e6ac9e15e0c
Hendi Santika <hendisantika@gmail.com>
2021-08-18 04:56:09 +0700

    #9 - Add BooksController

---
+++ src/main/java/com/hendisantika/controller/BooksController.java
@@ -0,0 +1,22 @@
+package com.hendisantika.controller;
+
+import com.hendisantika.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-binding-list-thymeleaf
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 18/08/21
+ * Time: 04.54
+ */
+@Controller
+@RequestMapping("/books")
+public class BooksController {
+    @Autowired
+    private BookService bookService;
+}
