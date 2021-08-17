0ab5b4bf12075de3a757818d1674ee4642303253
Hendi Santika <hendisantika@gmail.com>
2021-08-18 05:21:48 +0700

    #29 - Add saveEmployee in EmployeeController

--- src/main/java/com/hendisantika/controller/EmployeeController.java
+++ src/main/java/com/hendisantika/controller/EmployeeController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -44,6 +45,13 @@ public class EmployeeController {
     @PostMapping(path = "/requestpart", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     public ResponseEntity<Object> saveEmployee(@RequestPart Employee employee, @RequestPart MultipartFile document) {
         employee.setDocument(document);
+        employeeService.save(employee);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping(path = "/requestparam", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<Object> saveEmployee(@RequestParam String name, @RequestPart MultipartFile document) {
+        Employee employee = new Employee(name, document);
         employeeService.save(employee);
         return ResponseEntity.ok().build();
     }
