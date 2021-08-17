bb46a6c99b9b3b0a62591f4436a2f8e3e3a6e892
Hendi Santika <hendisantika@gmail.com>
2021-08-18 05:30:43 +0700

    #33 - Add givenFormData_whenPost_thenReturns200OK in EmployeeControllerTest

--- src/test/java/com/hendisantika/controller/EmployeeControllerTest.java
+++ src/test/java/com/hendisantika/controller/EmployeeControllerTest.java
@@ -1,6 +1,7 @@
 package com.hendisantika.controller;
 
 import com.hendisantika.service.EmployeeService;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -9,6 +10,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import static org.apache.http.entity.ContentType.DEFAULT_BINARY;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 
 /**
  * Created by IntelliJ IDEA.
@@ -30,4 +32,13 @@ class EmployeeControllerTest {
 
     @MockBean
     private EmployeeService employeeService;
+
+    @Test
+    public void givenFormData_whenPost_thenReturns200OK() throws Exception {
+        mockMvc.perform(multipart("/employees")
+                        .file(A_FILE)
+                        .param("name", "testname"))
+                .andExpect(status().isOk());
+    }
+
 }
