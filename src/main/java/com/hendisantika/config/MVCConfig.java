1db61fbe6daceed395434a56fc60ed57c2c3fca0
Hendi Santika <hendisantika@gmail.com>
2021-08-18 04:54:04 +0700

    #8 - Add templateResolver in MVCConfig

--- src/main/java/com/hendisantika/config/MVCConfig.java
+++ src/main/java/com/hendisantika/config/MVCConfig.java
@@ -1,10 +1,14 @@
 package com.hendisantika.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,5 +27,15 @@ public class MVCConfig implements WebMvcConfigurer {
         registry.addViewController("/")
                 .setViewName("index");
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    }
+
+    @Bean
+    public ITemplateResolver templateResolver() {
+        ClassLoaderTemplateResolver resolver = new ClassLoaderTemplateResolver();
+        resolver.setPrefix("templates/books/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode(TemplateMode.HTML);
+        resolver.setCharacterEncoding("UTF-8");
+        return resolver;
     }
 }
