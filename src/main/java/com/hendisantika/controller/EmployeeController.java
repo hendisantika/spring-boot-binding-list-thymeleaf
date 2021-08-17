a8718c0ca9bcd054df8afae9170f00db97b3267a
Hendi Santika <hendisantika@gmail.com>
2021-08-18 05:17:52 +0700

    #26 - Add showEmployeeForm in EmployeeController

--- src/main/java/com/hendisantika/controller/EmployeeController.java
+++ src/main/java/com/hendisantika/controller/EmployeeController.java
@@ -1,8 +1,12 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.employee.Employee;
 import com.hendisantika.service.EmployeeService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by IntelliJ IDEA.
@@ -15,6 +19,13 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 @AllArgsConstructor
+@RequestMapping(value = "/employees")
 public class EmployeeController {
     private final EmployeeService employeeService;
+
+    @GetMapping
+    public String showEmployeeForm(Model model) {
+        model.addAttribute("employee", new Employee());
+        return "employee/createEmployeeForm";
+    }
 }
