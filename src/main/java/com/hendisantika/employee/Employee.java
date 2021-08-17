091b3b5901d976d6985cba858a7ec8098f1fbfb7
Hendi Santika <hendisantika@gmail.com>
2021-08-18 05:10:22 +0700

    #19 - Add Employee Model

---
+++ src/main/java/com/hendisantika/employee/Employee.java
@@ -0,0 +1,25 @@
+package com.hendisantika.employee;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-binding-list-thymeleaf
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 18/08/21
+ * Time: 05.10
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Employee {
+    private String name;
+    private MultipartFile document;
+}
